2ac7b74b8da1e1f4e553cfecd6f8ae9e276b07dc
Hassan Mian <mian@willandskill.se>
2020-12-07 14:58:04 +0100

    Add ContainerComponents

---
+++ src/components/Container/PrimaryContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Container.module.scss'
+
+export default function PrimaryContainer() {
+    return (
+        <div className={styles.primary}>
+            This is a primary container
+        </div>
+    )
+}

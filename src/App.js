af39198b24c92f99cf64c92e7ef918804960b483
Hassan Mian <mian@willandskill.se>
2020-12-07 14:58:09 +0100

    Render new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import PrimaryButton from './components/Button/PrimaryButton'
+import SecondayButton from './components/Button/SecondayButton'
 import ButtonCSS from './components/ButtonCSS'
 import ButtonModule from './components/ButtonModule'
 import ButtonSCSS from './components/ButtonSCSS'
+import PrimaryContainer from './components/Container/PrimaryContainer'
+import SecondaryContainer from './components/Container/SecondaryContainer'
 
 function App() {
   return (
@@ -11,6 +15,11 @@ function App() {
       <ButtonSCSS />
       <ButtonModule />
       <button className="btn">Should not be green</button>
+      <br/>
+      <PrimaryButton />
+      <SecondayButton />
+      <PrimaryContainer />
+      <SecondaryContainer />
     </div>
   );
 }

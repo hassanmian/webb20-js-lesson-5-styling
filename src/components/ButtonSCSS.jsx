58666cabfe12e280b54f13e71106167d86563bb0
Hassan Mian <mian@willandskill.se>
2020-12-07 14:41:01 +0100

    Style using scss

---
+++ src/components/ButtonSCSS.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/button.scss'
+
+export default function ButtonSCSS() {
+    return (
+        <button className="btn button-scss">
+            Button SCSS
+        </button>
+    )
+}

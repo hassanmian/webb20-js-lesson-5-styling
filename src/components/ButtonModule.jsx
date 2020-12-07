6e5e153c1c2cbe41e685d7d3de2b4c5fd6f02162
Hassan Mian <mian@willandskill.se>
2020-12-07 14:41:08 +0100

    Style using modules

---
+++ src/components/ButtonModule.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/button.module.css'
+
+export default function ButtonModule() {
+  console.log(styles)
+  return (
+    <button className={styles.btn}>
+      Button Module
+    </button>
+  )
+}
